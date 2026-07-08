58713735ff4cf486c44b5a752b3d6e9f1dcbc581
sehajveersingh136-byte <shejveersingh136@gmail.com>
2026-07-08 16:43:26 +0530

    Modify model.py

--- model.py
+++ model.py
@@ -5,9 +5,9 @@ class Task(Base):
     __tablename__ = "tasks"
 
     id = Column(Integer, primary_key=True, index=True)
-    task_title = Column(String, nullable=False)
+    task_title = Column(String, unique=True)
     description = Column(String)
-    assigned_to = Column(String)
+    assigned_to = Column(String)   # Changed from Integer
     priority = Column(String)
     status = Column(String)
     due_date = Column(String)
